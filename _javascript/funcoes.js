3f73e58702db6efa6c67bb7b99c0aa1231bf4997
IrPedro <99945573+irpedro@users.noreply.github.com>
2022-03-25 03:09:43 -0300

    Delete _javascript directory

--- _javascript/funcoes.js
+++
@@ -1,8 +0,0 @@
-function mudaFoto (foto) {
-    document.getElementById("icone").src = foto;
-}
-function calc_total() {
-    var qtd = parseInt(document.getElementById('cQuant').value);
-    tot = qtd * 1500;
-    document.getElementById('cTot').value = tot;
-}
