9c78279a57b6554e639c4226154bd2cbd677b0b1
IrPedro <99945573+irpedro@users.noreply.github.com>
2022-03-25 02:50:39 -0300

    Add files via upload

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,1 +1,8 @@
-
+function mudaFoto (foto) {
+    document.getElementById("icone").src = foto;
+}
+function calc_total() {
+    var qtd = parseInt(document.getElementById('cQuant').value);
+    tot = qtd * 1500;
+    document.getElementById('cTot').value = tot;
+}
